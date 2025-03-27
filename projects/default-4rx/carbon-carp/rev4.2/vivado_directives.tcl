22d09721a86b7641e3ca6015e8ca12c63705d985
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2025-03-27 09:00:36 -0400

    Update to help meet timing

--- projects/default-4rx/carbon-carp/rev4.2/vivado_directives.tcl
+++ projects/default-4rx/carbon-carp/rev4.2/vivado_directives.tcl
@@ -6,8 +6,8 @@ set board [string range $full_board 4 6]
 if { ( $board == "9eg" ) } {
     set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
     set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreWithRemap [get_runs impl_1]
-    set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE NoTimingRelaxation [get_runs impl_1]
-    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE SSI_HighUtilSLRs [get_runs impl_1]
+    set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
     set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
 }
 
