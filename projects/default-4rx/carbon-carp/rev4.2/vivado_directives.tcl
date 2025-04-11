ed3d259d86c5bd3a58a55865dbcc5c61eae3a833
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2025-04-11 14:41:54 -0400

    Update to meet timing

--- projects/default-4rx/carbon-carp/rev4.2/vivado_directives.tcl
+++ projects/default-4rx/carbon-carp/rev4.2/vivado_directives.tcl
@@ -5,10 +5,11 @@ set board [string range $full_board 4 6]
 
 if { ( $board == "9eg" ) } {
     set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
-    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreWithRemap [get_runs impl_1]
+    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraPostPlacementOpt [get_runs impl_1]
+    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
     set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
-    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
-    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
+    set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
 }
 
 set full_board [get_parts -of_objects [get_projects]]
